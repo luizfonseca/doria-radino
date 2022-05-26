abd2b8695e46c2f799716974e75b95a5c07d2cc1
Daniel Sperling <daniel_sperling@yahoo.com>
2022-05-26 22:24:13 +0200

    new style

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Home({ contentful }) {
               id="home-image"
               style={{
                 width: "100%",
-                height: "800px",
+                height: "560px",
                 background: `url('/OlioRadinoLogo.jpg') no-repeat center`,
                 backgroundSize: "cover"
 
